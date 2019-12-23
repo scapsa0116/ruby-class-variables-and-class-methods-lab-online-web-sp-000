b26995c8eaea25cdc2ad3c87d9a88c1a5b392385
Stela Capsa <stela.capsa@gmail.com>
2019-12-23 15:45:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,9 @@ class Song
   @@genres = []
  
  def initialize (name, artist, genre)
-   @@name = name
-   @@artist = artist
-   @@genre = genre
+   @name = name
+   @artist = artist
+   @genre = genre
    @@count +=1 
    @@artists << artist 
    @@genres << genre 

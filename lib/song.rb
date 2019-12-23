0321964a348c84594e740484cb8d3a8eac1c0915
Stela Capsa <stela.capsa@gmail.com>
2019-12-23 15:14:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,8 @@ class Song
      if genre_count|genre|
        genre_count|genre| +=1
        else 
-         genre_count|genre| = 1
+         genre_count|genre| = 1 
+       end
        
    
 end

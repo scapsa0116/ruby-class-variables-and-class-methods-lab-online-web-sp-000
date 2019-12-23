aae62467a8fa769a0e9e7f6ac00739d9a12270cf
Stela Capsa <stela.capsa@gmail.com>
2019-12-23 15:07:47 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+ attr_accessor:name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+ 
+ def initialize(name, artist, genre)
+   @@name = name
+   @@artist = artist
+   @@genre = genre
+   @@count +=1 
+   @@artists << artist 
+   @@genres << genre 
+ end
+ def self.count
+   @@count
+ end
+end

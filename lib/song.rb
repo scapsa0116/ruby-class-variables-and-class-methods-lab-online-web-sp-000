e1e0067f4f746db08e31b2d8655aebc242dc8480
Stela Capsa <stela.capsa@gmail.com>
2019-12-23 15:18:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
  
  def self.genre_count
    genre_count = {}
-   genres.each do |genre|
+   @@genres.each do |genre|
      if genre_count|genre|
        genre_count|genre| +=1
        else 
